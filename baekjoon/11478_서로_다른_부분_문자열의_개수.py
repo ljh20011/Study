7687aa76331b35d7510ab05e1144f7496fe21920
JongHyuk <ljh20011@naver.com>
2023-11-20 01:16:42 +0900

    baekjoon

--- baekjoon/11478_서로_다른_부분_문자열의_개수.py
+++ baekjoon/11478_서로_다른_부분_문자열의_개수.py
@@ -1,18 +1,9 @@
-import numpy as np
 def sol(s):
     ans = set()
     for i in range(1, len(s)+1): # 문자열의 길이
         for j in range(0,len(s)): # 시작점
             ans.add(s[j:j+i])
     return len(ans)
-
-array1 = np.arange(10)
-array4 = array1.reshape(-1, 2)
-print('array4:\n', array4)
-
-array5 = array1.reshape(2, -1)
-print(len(array5))
-print('array5:\n', array5)
 
 s = input()
 print(sol(s))
